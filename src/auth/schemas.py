6c6a4c52b649aff9fa242d413248531dfd90898f
xryak <sergey.igymn@gmail.com>
2025-06-22 18:07:37 +0300

    Хэширование пароля перенесено в сехму вместо репозитория

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr, field_validator
 
+from src.users.models import hash_password
 from src.users.schemas import UserRead
 
 
@@ -16,6 +17,12 @@ class UserLogin(BaseModel):
         if not v:
             raise ValueError("Must not be empty string and must be a email")
         return v
+
+    @field_validator("password", mode="before")
+    @classmethod
+    def hash(cls, v):
+        """Hash the password before storing."""
+        return hash_password(str(v))
 
     @field_validator("password")
     @classmethod
