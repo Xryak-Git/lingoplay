3940ba7df95eac3a7ce4a4622e8fe6862f83a7d4
xryak <sergey.igymn@gmail.com>
2025-06-28 12:41:20 +0300

    test: При поднятии тестовой базы создается тестовый пользователь

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,15 +7,35 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 
 from src.database.core import Base, get_session
 from src.main import app
+from src.users.models import LingoplayUser
 
 TEST_DB_URI = "sqlite+aiosqlite:///:memory:"
+
+TEST_USER_EMAIL = "test@example.com"
+TEST_USERNAME = "Test User"
+TEST_USER_PASSWORD = "123"
+
 
 engine_test = create_async_engine(TEST_DB_URI, echo=False)
 async_session_maker = async_sessionmaker(engine_test, expire_on_commit=False)
 
+
 async def override_get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def exsisting_user(prepare_test_db) -> AsyncGenerator[LingoplayUser, None]:
+    """Создаёт тестового пользователя перед всеми тестами"""
+    async for session in override_get_session():
+        user = LingoplayUser(email=TEST_USER_EMAIL, username=TEST_USERNAME, password=bytes(TEST_USER_PASSWORD, "utf-8"))
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+
+    yield user
+
 
 @pytest_asyncio.fixture(scope="session")
 async def prepare_test_db():
@@ -24,6 +44,7 @@ async def prepare_test_db():
     yield
     async with engine_test.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
+
 
 @pytest_asyncio.fixture(scope="function")
 async def client(prepare_test_db):
