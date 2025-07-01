43a68fe19d1b6183269928b4f9f1230235ece174
xryak <sergey.igymn@gmail.com>
2025-07-01 17:42:20 +0300

    test(uploads): Тесты на get ручки

--- tests/fixtures/db.py
+++ tests/fixtures/db.py
@@ -5,7 +5,7 @@ import pytest_asyncio
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from src.database.core import Base
-from src.uploads.models import Games
+from src.uploads.models import Games, Videos
 from src.users.models import LingoplayUsers
 from src.users.schemas import UserCreate
 from tests.constants import TEST_DB_URI, TEST_USER_EMAIL, TEST_USER_PASSWORD, TEST_USERNAME
@@ -33,15 +33,30 @@ async def exsisting_user(prepare_test_db) -> AsyncGenerator[LingoplayUsers, None
 
 
 @pytest_asyncio.fixture(scope="session", autouse=True)
-async def existing_game(prepare_test_db) -> AsyncGenerator[Games, None]:
+async def existing_game(prepare_test_db, exsisting_user: LingoplayUsers) -> AsyncGenerator[Games, None]:
     """Создаёт тестовыую игру всеми тестами"""
     async for session in override_get_session():
         game = Games(title="kek")
+        game.users.append(exsisting_user)
         session.add(game)
         await session.commit()
         await session.refresh(game)
 
     yield game
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def existing_video(
+    prepare_test_db, exsisting_user: LingoplayUsers, existing_game: Games
+) -> AsyncGenerator[Videos, None]:
+    """Создаёт тестовыую игру всеми тестами"""
+    async for session in override_get_session():
+        video = Videos(title="pek", path="video.mp4", user_id=exsisting_user.id, game_id=existing_game.id)
+        session.add(video)
+        await session.commit()
+        await session.refresh(video)
+
+    yield video
 
 
 @pytest_asyncio.fixture(scope="session")
--- tests/uploads/test_uploads.py
+++ tests/uploads/test_uploads.py
@@ -4,17 +4,15 @@ import pytest
 from httpx import AsyncClient
 
 from src.repository import AbstractS3Repository
-from src.uploads.models import Games
+from src.uploads.models import Games, Videos
 from src.uploads.schemas import GameCreate
 
 
 class TestUploadsRoutes:
-
     @pytest.mark.asyncio
     async def test_video_upload(
         self, client: AsyncClient, existing_game: Games, s3_test_repo: AbstractS3Repository, logined_user_headers: dict
     ):
-
         fake_video = io.BytesIO(b"fake mp4 data")
         fake_video.name = "test_video.mp4"
 
@@ -47,3 +45,42 @@ class TestUploadsRoutes:
         )
 
         assert response.status_code == 200, "Persona 5 Royal" in response.text
+
+    @pytest.mark.asyncio
+    async def test_get_users_videos(self, client: AsyncClient, logined_user_headers: dict, existing_video: Videos):
+        response = await client.get("/uploads/videos", headers=logined_user_headers)
+
+        assert response.status_code == 200
+        assert existing_video.title in response.json()["list"][0].get("title", "")
+
+    @pytest.mark.asyncio
+    async def test_get_users_video(self, client: AsyncClient, logined_user_headers: dict, existing_video: Videos):
+        response = await client.get(f"/uploads/videos/{existing_video.id}", headers=logined_user_headers)
+
+        assert response.status_code == 200
+        data = response.json()
+        assert "id" in data
+        assert data["id"] == existing_video.id
+
+    @pytest.mark.asyncio
+    async def test_get_all_games(self, client: AsyncClient, logined_user_headers: dict, existing_game: Games):
+        response = await client.get("/uploads/games?all=true", headers=logined_user_headers)
+        response_list = response.json()["list"]
+        assert response.status_code == 200
+        assert isinstance(response_list, list)
+        assert any(existing_game.title in item["title"] for item in response_list)
+
+    @pytest.mark.asyncio
+    async def test_get_filtered_games_by_title(self, client: AsyncClient, logined_user_headers: dict, existing_game: Games):
+        response = await client.get(f"/uploads/games?title={existing_game.title}", headers=logined_user_headers)
+
+        assert response.status_code == 200
+        games = response.json()["list"]
+        assert all(existing_game.title in game["title"] for game in games)
+
+    @pytest.mark.asyncio
+    async def test_get_users_game_by_id(self, client: AsyncClient, logined_user_headers: dict, existing_game: Games):
+        response = await client.get(f"/uploads/games/{existing_game.id}", headers=logined_user_headers)
+
+        assert response.status_code == 200
+        assert response.json()["id"] == existing_game.id
