e88e11f62f3f2ce98b7d049bc944e9cc4a5c038c
xryak <sergey.igymn@gmail.com>
2025-07-04 21:07:02 +0300

    refactor(uploads): Переработки репозиторя загрузок

--- src/uploads/repository.py
+++ src/uploads/repository.py
@@ -20,35 +20,50 @@ class VideoRepository(AlchemyRepository):
         self._s3_repository = s3_repository
 
     async def create_one(self, data: VideoCreate) -> Videos:
-        dir_path = f"{data.user_id}/{self._dir_name}/{data.title}"
-        video_path = f"{dir_path}/{data.title}{Path(data.video.filename).suffix}"
-        thumblnail_path = f"{dir_path}/thumblnail.png"
+        dir_path = self._build_dir_path(data.user_id, data.title)
+        video_path = self._build_video_path(dir_path, data.video.filename)
+        thumbnail_path = f"{dir_path}/thumbnail.png"
 
         if await self.exists(path=video_path):
             raise AlreadyExistsError(self.model.__tablename__, "path", video_path)
 
         video_url = await self._s3_repository.upload_file(data.video.file, video_path)
-        thumblnail_url = (
-            await self._s3_repository.upload_file(data.thumblnail, thumblnail_path) if data.thumblnail else None
-        )
+        thumbnail_url = await self._upload_thumbnail(data.thumblnail, thumbnail_path)
 
         async with self._session as session:
-            result = await session.execute(select(Games).where(Games.id == data.game_id))
-            game = result.scalars().one()
-
+            game = await self._get_game_by_id(session, data.game_id)
             video = Videos(
-                user_id=data.user_id, path=video_url, thumblnail_path=thumblnail_url, title=data.title, game=game
+                user_id=data.user_id,
+                title=data.title,
+                path=video_url,
+                thumblnail_path=thumbnail_url,
+                game=game,
             )
             session.add(video)
             await session.commit()
             await session.refresh(video)
             return video
 
-    async def exists(self, path: str):
+    async def exists(self, path: str) -> bool:
         async with self._session as session:
             stmt = select(exists().where(Videos.path.like(f"%{path}")))
             result = await session.execute(stmt)
             return result.scalar()
+
+    def _build_dir_path(self, user_id: int, title: str) -> str:
+        return f"{user_id}/{self._dir_name}/{title}"
+
+    def _build_video_path(self, dir_path: str, filename: str) -> str:
+        return f"{dir_path}/{Path(filename).name}"
+
+    async def _upload_thumbnail(self, thumblnail: bytes | None, path: str) -> str | None:
+        if not thumblnail:
+            return None
+        return await self._s3_repository.upload_file(thumblnail, path)
+
+    async def _get_game_by_id(self, session: AsyncSession, game_id: int) -> Games:
+        query = await session.execute(select(Games).where(Games.id == game_id))
+        return query.scalars().one()
 
 
 class GamesRepository(AlchemyRepository):
